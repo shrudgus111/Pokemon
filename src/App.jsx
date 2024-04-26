3ee2f7ec5fca0236d1140d101ffb3a4342eca550
shrudgus111 <roh3383@gmail.com>
2024-04-26 16:36:16 +0900

    Resolved merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,7 @@ import {Route, Routes}  from 'react-router-dom'
 import Layout from '@/Layout'
 import HomeView from '@/views/HomeView'
 import ProductView from '@/views/ProductView'
-<<<<<<< HEAD
 import CartView from '@/views/CartView'
-=======
-import ProductDetailView from '@/views/ProductDetailView'
-import ProductModifyView from '@/views/ProductModifyView'
-import ProductInsertView from '@/views/ProductInsertView'
-import NewsView from '@/views/NewsView'
-import EventView from '@/views/EventView'
-import CardView from '@/views/CardView';
->>>>>>> origin/main
 import LoginView from '@/views/LoginView'
 import JoinView from '@/views/JoinView'
 import ApplicationView from '@/views/ApplicationView';
@@ -26,15 +17,12 @@ const App = () => {
         <Route index element={ <HomeView /> } />
         <Route path="/news" element={ <NewsView />} />
         <Route path="/product" element={ <ProductView />} />
-<<<<<<< HEAD
         <Route path="/cart" element={ <CartView />} />
-=======
         <Route path="/product/:id" element={ <ProductDetailView />} />
         <Route path="/productModify" element={ <ProductModifyView /> } />
         <Route path="/productInsert" element={ <ProductInsertView />} />
         <Route path="/search" element={ <CardView />} />
         <Route path="/event" element={ <EventView />} />
->>>>>>> origin/main
         <Route path="/login" element={ <LoginView /> } />
         <Route path="/join" element={ <JoinView /> } />
         <Route path="/application" element={ <ApplicationView /> } />
