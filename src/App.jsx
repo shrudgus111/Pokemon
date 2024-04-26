06d1ff85f2ab8c1f530143e83e05ddd85921fc2f
shrudgus111 <roh3383@gmail.com>
2024-04-26 16:28:40 +0900

    4-26

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import {Route, Routes}  from 'react-router-dom'
 import Layout from '@/Layout'
 import HomeView from '@/views/HomeView'
 import ProductView from '@/views/ProductView'
-import ProductDetailView from '@/views/ProductDetailView'
-import ProductModifyView from '@/views/ProductModifyView'
-import ProductInsertView from '@/views/ProductInsertView'
+import CartView from '@/views/CartView'
 import LoginView from '@/views/LoginView'
 import JoinView from '@/views/JoinView'
 import ApplicationView from '@/views/ApplicationView';
+import PlayView from '@/views/PlayView';
 // import MemberModifyView from '@/views/MemberModifyView'
 
 const App = () => {
@@ -17,12 +16,11 @@ const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={ <HomeView /> } />
         <Route path="/product" element={ <ProductView />} />
-        <Route path="/product/:id" element={ <ProductDetailView />} />
-        <Route path="/productModify" element={ <ProductModifyView /> } />
-        <Route path="/productInsert" element={ <ProductInsertView />} />
+        <Route path="/cart" element={ <CartView />} />
         <Route path="/login" element={ <LoginView /> } />
         <Route path="/join" element={ <JoinView /> } />
         <Route path="/application" element={ <ApplicationView /> } />
+        <Route path="/play" element={ <PlayView /> } />
         {/* <Route path="/memberModify" element={ <MemberModifyView /> } /> */}
         
       </Route>
--- src/components/product/ProductCategory.jsx
+++ src/components/product/ProductCategory.jsx
@@ -4,22 +4,28 @@ import styled from 'styled-components'
 const ProductCategoryBlock = styled.div`
     display: flex;
     justify-content: center;
+    border-bottom:1px solid #000;
     button {
-        width: 100px;
-        height: 40px;
-        margin: 0 20px;
-        background: blue;
-        color: #fff;
+        width:300px;
+        font-size:25px;
+        margin:0 100px;
+        padding-bottom: 15px;
+      
+        background:#fff;
+        border-bottom:1px solid #000;
         &.on {
-          background: red;
+            background:#fff;
+            color:#ffa70e;
+            border-bottom:5px solid #ffa70e;
+            font-weight:700;
         }
       }
 `
 
 const ProductCategory = ({changeTitle, title}) => {
-    const category = ['all', 'woman', 'man', 'underwear', 'kids']
+    const category = ['전체상품', '구축덱', '특별제품', ]
     return (
-        <ProductCategoryBlock>
+        <ProductCategoryBlock className='row'>
             {
                 category.map((item, index)=>(
                     <button key={index} type="button" onClick={()=>changeTitle(item)} className={ title==item && "on" }>{item}</button>
